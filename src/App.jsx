3f1489ec9212da70ebefbbee7a460d24ef7fee17
Shivam <shivammishraanil@gmail.com>
2024-08-15 01:51:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="logo-container">
+        <div className="diamond">
+          <div className="line box1"></div>
+          <div className="line box2"></div>
+          <div className="line box3"></div>
+          <div className="line box4"></div>
+        </div>
+        <div className="text-container">
+          <div className="text">
+            HTML <span className="and">&</span> CSS <br />
+            <div className="sub">Design and Build Websites</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
